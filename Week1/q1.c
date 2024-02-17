dbde27ecc3437f65920ac66aed70858245892ad6
Afrxz <146107723+Afrxz@users.noreply.github.com>
2024-02-17 16:52:56 +0400

    Update q1.c

--- Week1/q1.c
+++ Week1/q1.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
-
 int
 main ()
 {
@@ -25,8 +22,24 @@ main ()
 	  }
 	top = top + 1;
 	strcpy (inp_array[top], inp);	// copy string from inp to inp_array[top]
-	
+
   }
+  void pop ()
+  {
+	if (top == -1)
+	  {
+		printf ("underflow");
+
+	  }
+	else
+	  {
+		printf ("popped element %s",inp_array[top]);
+		top = top - 1;
+	  }
+  }
+
+
+
   fptr = fopen ("studentin.dat", "r");
 
   if (fptr == NULL)
@@ -39,6 +52,34 @@ main ()
 	{
 	  push (a);
 	  printf ("%s", a);
-	}
-}
 
+	}
+
+  printf ("done\n");
+  void show ()
+  {
+
+	if (top == -1)
+	  {
+		printf ("\nUnderflow!!");
+	  }
+	else
+	  {
+		printf ("\nElements present in the stack: \n");
+		fptr = fopen ("studentout.dat", "w");
+		for (int i = top; i >= 0; --i)
+		  {
+			printf ("%s\n", inp_array[i]);
+			
+			
+			fprintf (fptr, "%s", inp_array[i]);
+		  }
+
+	  }
+
+
+
+
+  }
+  pop();
+  show();
